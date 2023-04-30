64f854f6c4be7cc6d7e4dbb7bd62fb831f1f499e
Jonah <jonahtkane@gmail.com>
2023-04-30 13:15:09 -0500

    added put method to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log("PUT to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: id, jate: content });
+  const result = awaite request;
+  console.log("Data saved to database", result);
+};
+
+
+console.error('putDb not implemented');
 
 // example from in class activities
 // export const putDb = async (id, content) => {
